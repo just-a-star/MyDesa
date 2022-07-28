7e159f990bdd69d3f73a96b3f61b6e845e1a6933
Bintang Rahmatullah <rahmatbintang181@gmail.com>
2022-07-28 15:42:40 +0700

    Menambahkankan fitur Resi

--- MyDesa/src/Controller/MenuAplikasiC.java
+++ MyDesa/src/Controller/MenuAplikasiC.java
@@ -119,6 +119,20 @@ public class MenuAplikasiC {
             e.printStackTrace();
         }
 
+    }
+
+    @FXML
+    void toPrint(MouseEvent event) {
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/pendataanForm.fxml"));
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
