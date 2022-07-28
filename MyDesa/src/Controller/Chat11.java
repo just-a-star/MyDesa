d72cbddaa1c7faba05e191f5ed0e4cc4ea9c508e
Mariimo <108393914+Mariimo@users.noreply.github.com>
2022-07-28 15:33:07 +0700

    add new chat and viewdok

---
+++ MyDesa/src/Controller/Chat11.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+/**
+ *
+ * @author Xeesh
+ */
+public class Chat11 {
+    private String msg;
+
+    public String getMsg() {
+
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
